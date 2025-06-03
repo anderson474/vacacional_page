f045fcb413f97fa2263c9eb0d9fa0e23dc89b943
Jhon Anderson Galeano Santillana <andersongs.474@gmail.com>
2025-06-03 07:40:28 -0500

    cambio de enlace de whatsapp por el de DXS solutions

--- components/cta.jsx
+++ components/cta.jsx
@@ -8,7 +8,7 @@ export default function CTA() {
       <section id="cta" className="bg-gray-200 py-16 text-center">
         <h2 className="text-3xl font-bold mb-4">¡Asegura tu cupo hoy mismo!</h2>
         <a
-          href="https://wa.me/573054240301"
+          href="https://wa.me/573052360841"
           target="_blank"
           className="inline-block bg-[#96b422] text-white px-6 py-3 rounded shadow hover:bg-gray-800"
         >
